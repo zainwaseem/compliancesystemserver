0b49fd2c037a41d54b2609614b65dc0ae910ba6b
zainwaseem <zainwaseem9371@gmail.com>
2023-07-09 22:06:20 +0500

    dev 32

--- HeaderUrl.js
+++ HeaderUrl.js
@@ -1,4 +1,5 @@
-export const HEADER_URL = "http://localhost:3000";
+export const HEADER_URL = "https://compliancesys.netlify.app/";
+// export const HEADER_URL = "http://localhost:3000";
 // export const BACKEND_URL = "https://compliancesystemserver.vercel.app/api/";
 // export const BACKEND_URL =
 //   "https://compliancesystemserver-production.up.railway.app/api/";
